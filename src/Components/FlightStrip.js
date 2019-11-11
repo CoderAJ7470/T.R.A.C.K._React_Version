75e98dbd9a99a1ab4c6f706cceb4549c106807d5
CoderAJ7470 <ameyasureshjoshi@gmail.com>
2019-11-11 16:30:43 -0600

    Implements drag functionality; still needs drop functionality

--- src/Components/FlightStrip.js
+++ src/Components/FlightStrip.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import { Draggable } from "react-beautiful-dnd";
 
+import flightStripStyles from '../CSS/flightStrip.module.css';
+
 export default class FlightStrip extends Component {
   constructor(props) {
     super(props);
-
-    // this.state = {
-    //   fltInfo: this.props.fltInfo
-    // }
   }
 
   render() {
     return (
-      <Draggable draggableId={this.props.key} index={this.props.index}>
+      <Draggable draggableId={this.props.fltKey} index={this.props.index}>
         {(provided) => (
-          <div
+          <div className={this.props.status === 'Departure' ? flightStripStyles['depFltStrip'] : flightStripStyles['arrFltStrip']}
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
           >
             <div>
-              {console.log('key is ', this.props.key)}
               {this.props.code} {this.props.number}
             </div>
             <div>
--- src/Components/Track_Panel.js
+++ src/Components/Track_Panel.js
@@ -8,7 +8,7 @@ export default function Header() {
   const [isDeparture, setStatus] = useState(true);
   const [statusButtonText, setButtonText] = useState('Departure');
 
-  const [id, setID] = useState(0);
+  let [id, setID] = useState("");
   const [codeInput, setCode] = useState('');
   const [fltNoInput, setFltNo] = useState('');
   const [acftTypeInput, setAcftType] = useState('');
@@ -44,10 +44,22 @@ export default function Header() {
   }
 
   function onDragEnd(result) {
-    // re-order the column to which the item was dragged
+    const { destination, source, draggableId } = result;
+
+    if(!destination) {
+      return;
+    }
+
+    if(destination.droppableId === source.droppableId && destination.index == source.index) {
+      return;
+    }
+
+    
   }
 
   function createFlightStrip() {
+    createID();
+
     if(statusButtonText === 'Departure') {
       addDepartureStrip();
     }
@@ -56,23 +68,40 @@ export default function Header() {
     }
 
     clearInputs();
+    document.getElementById('codeInput').focus();
+  }
+
+  const createID = () => {
+    id = id + '1';
+    setID(id);
   }
 
   // create a departures object and push it to the departures array
   function addDepartureStrip() {
     updateDepArray([...departuresArray,
       {
-        id: setID(id + 1),
+        id: id,
         code: codeInput,
         fltNum: fltNoInput,
         type: acftTypeInput,
-        cruiseAlt: cruiseAltInput
+        cruiseAlt: cruiseAltInput,
+        status: statusButtonText
       }
     ]);
   }
 
+  // create a departures object and push it to the departures array
   function addArrivalStrip() {
-    // create an arrival strip and add to the arrivals array
+    updateArrArray([...arrivalsArray,
+      {
+        id: id,
+        code: codeInput,
+        fltNum: fltNoInput,
+        type: acftTypeInput,
+        cruiseAlt: cruiseAltInput,
+        status: statusButtonText
+      }
+    ]);
   }
 
   function clearInputs() {
@@ -96,7 +125,7 @@ export default function Header() {
           <legend>Generate a flight strip...</legend>
 
           <input type="text" value={codeInput} onChange={e => setCode(e.target.value)}
-            className={trackPanelStyles['codeInput']}
+            className={trackPanelStyles['codeInput']} id='codeInput'
             placeholder='Enter a code...' />
 
           <input type="text" value={fltNoInput} onChange={e => setFltNo(e.target.value)}
@@ -126,24 +155,46 @@ export default function Header() {
         </fieldset>
       </div>
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId={columnDepID}>
-          {(provided) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              >
-                {departuresArray.map((fltData, index) =>
-                  <FlightStrip
-                    index={index}
-                    key={fltData.id}
-                    code={fltData.code}
-                    number={fltData.fltNum}
-                    type={fltData.type}
-                    altitude={fltData.cruiseAlt} />)}
-                {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+        <div className={trackPanelStyles['flightStripTable']}>
+          <Droppable droppableId={columnDepID}>
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                >
+                  {departuresArray.map((fltData, index) =>
+                    <FlightStrip
+                      index={index}
+                      fltKey={fltData.id}
+                      code={fltData.code}
+                      number={fltData.fltNum}
+                      type={fltData.type}
+                      altitude={fltData.cruiseAlt}
+                      status={fltData.status} />)}
+                  {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <Droppable droppableId={columnArrID}>
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                >
+                  {arrivalsArray.map((fltData, index) =>
+                    <FlightStrip
+                      index={index}
+                      fltKey={fltData.id}
+                      code={fltData.code}
+                      number={fltData.fltNum}
+                      type={fltData.type}
+                      altitude={fltData.cruiseAlt}
+                      status={fltData.status} />)}
+                  {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
       </DragDropContext>
     </>
   )
